863609c4387e796872d73c3edab528079792e297
Will Pragnell <wpragnell@gmail.com>
2013-04-04 20:10:54 +0100

    refactor processing_doc.py a bit

--- python/processing_doc.py
+++ python/processing_doc.py
@@ -24,32 +24,31 @@ def wordStart(line, column):
             break
     return start
 
+def isFunction():
+    (row, col) = vim.current.window.cursor
+    line = vim.current.line
+    col = wordStart(line, col)
+    if re.match(r"\w+\s*\(", line[col:]):
+        if col < 4:
+            return True
+        else:
+            col -= 4
+            if re.match(r"new\s*\w+\s*\(", line[col:]):
+                return False
+            else:
+                return True
+    else:
+        return False
+
 if vim.eval("g:processing_doc_style") == "local":
     basepath = path.abspath(vim.eval("g:processing_doc_path"))
     launchDoc = launchDocFile
 else:
     launchDoc = launchDocWeb
 
-(row, col) = vim.current.window.cursor
-line = vim.current.line
-
-col = wordStart(line, col)
-if re.match(r"\w+\s*\(", line[col:]):
-    if col < 4:
-        isFunction = True
-    else:
-        col -= 4
-        if re.match(r"new\s*\w+\s*\(", line[col:]):
-            isFunction = False
-        else:
-            isFunction = True
-else:
-    isFunction = False
-
 word = vim.eval('expand("<cword>")')
-
 if word:
-    if isFunction:
+    if isFunction():
         success = launchDoc(word + "_.html") or launchDoc(word + ".html")
     else:
         success = launchDoc(word + ".html") or launchDoc(word + "_.html")
